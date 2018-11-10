bece7d34dfdac8b554ba796635eb638a207f997a
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-11-10 11:41:53 +0100

    update

--- JS/JWT.js
+++ JS/JWT.js
@@ -1,9 +1,63 @@
 (function($){
 
-	const RESTROOT = 'http://restful.dev/wp-json';
+	const RESTROOT = 'https://dwsdemo2.preludio.nl/wp-json';
 	const $ENTRYTITLE = $('.post-title');
 	const $LOGIN = $('#loginform');
 	const $LOGOUT = $('#logout');
+
+	// Add edit post functionality:
+	function editPost() {
+
+	    $ENTRYTITLE.after( '<button class="edit-button edit-title">Edit title</button><button class="edit-title save" style="display: none">Save title</button>' );
+
+	    $('.edit-title.edit-button').click(function(){
+	        let $originalTitle = $ENTRYTITLE.text();
+	        $ENTRYTITLE.toggle();
+	        $ENTRYTITLE.after('<input id="title-input" type="text">');
+	        document.querySelector('#title-input').value = $originalTitle;
+	        $(this).toggle();
+	        $('.edit-title.save').toggle();
+	    });
+
+	    $('.save').click(function(){
+			let postID = document.querySelector('.post').getAttribute('data-id');
+	        let newTitle = document.querySelector('#title-input').value;
+	    });
+
+	}
+	
+	// Get a new token, store it in sessionStorage:
+	function getToken(username,password) {
+
+		$.ajax({
+			url: RESTROOT + '/jwt-auth/v1/token',
+			method: 'POST',
+			data:{
+				'username': username,
+				'password': password
+			}
+		})
+
+		.done(function(response){
+			sessionStorage.setItem('newToken',response.token);
+			$LOGIN.toggle();
+			$LOGOUT.toggle();
+			editPost();
+		})
+
+		.fail(function(response){
+			console.error("REST error.");
+		})
+	}
+
+	// Clear token from sessionStorage:
+	function clearToken() {
+		sessionStorage.removeItem('newToken');
+		$LOGIN.toggle();
+		$LOGOUT.toggle();
+	}
+
+
 
 	$LOGIN.toggle();
 	$('#login_button').click(function(e){
@@ -11,6 +65,9 @@
 		let username = document.querySelector('#user_login').value;
 		let password = document.querySelector('#user_pass').value;
 		console.info("Username: " + username + " Password: " + password);
+		getToken(username,password);
 	});
+
+	$('#logout').click(clearToken);
 
 })(jQuery);
